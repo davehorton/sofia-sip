51bb86448d952a5997a8d7e38c545c164fa112a2
Dave Horton <daveh@beachdognet.com>
2014-04-24 10:11:50 -0400

    fix invalid memory reference that failed on fedora20 and mint

--- libsofia-sip-ua/msg/msg_parser.c
+++ libsofia-sip-ua/msg/msg_parser.c
@@ -2539,17 +2539,37 @@ msg_hclass_offset(msg_mclass_t const *mc, msg_pub_t const *mo, msg_hclass_t *hc)
 
   if (hc->hc_hash > 0) {
     unsigned j, N = mc->mc_hash_size;
-    for (j = hc->hc_hash % N; mc->mc_hash[j].hr_class; j = (j + 1) % N)
+    for (j = hc->hc_hash % N; mc->mc_hash[j].hr_class; j = (j + 1) % N) {
       if (mc->mc_hash[j].hr_class == hc) {
-	return (msg_header_t **)((char *)mo + mc->mc_hash[j].hr_offset);
+        return (msg_header_t **)((char *)mo + mc->mc_hash[j].hr_offset);
       }
-  }
-  else
+    }
+   }
+  else {
     /* Header has no name. */
-    for (i = 0; i <= 6; i++)
-      if (hc->hc_hash == mc->mc_request[i].hr_class->hc_hash)
-	return (msg_header_t **)((char *)mo + mc->mc_request[i].hr_offset);
-
+    int mine = hc->hc_hash ;
+    if( mine == mc->mc_payload[0].hr_class->hc_hash ) {
+       return (msg_header_t **)((char *)mo + mc->mc_payload[0].hr_offset);
+    }
+    else if( mine == mc->mc_request[0].hr_class->hc_hash ) {
+       return (msg_header_t **)((char *)mo + mc->mc_request[0].hr_offset);
+    }
+    else if( mine == mc->mc_error[0].hr_class->hc_hash ) {
+       return (msg_header_t **)((char *)mo + mc->mc_error[0].hr_offset);
+    }
+    else if( mine == mc->mc_status[0].hr_class->hc_hash ) {
+       return (msg_header_t **)((char *)mo + mc->mc_status[0].hr_offset);
+    }
+    else if( mine == mc->mc_unknown[0].hr_class->hc_hash ) {
+       return (msg_header_t **)((char *)mo + mc->mc_unknown[0].hr_offset);
+    }
+    else if( mine == mc->mc_multipart[0].hr_class->hc_hash ) {
+       return (msg_header_t **)((char *)mo + mc->mc_multipart[0].hr_offset);
+    }
+    else if( mine == mc->mc_separator[0].hr_class->hc_hash ) {
+       return (msg_header_t **)((char *)mo + mc->mc_separator[0].hr_offset);
+    }
+  }
   return NULL;
 }
 
