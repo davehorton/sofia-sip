c91873d98aab5faf9744425e710f552b0d0e0a24
Dave Horton <daveh@beachdognet.com>
2017-01-21 00:29:03 -0500

    fix compile error in prev checkin

--- libsofia-sip-ua/nta/nta.c
+++ libsofia-sip-ua/nta/nta.c
@@ -9315,10 +9315,10 @@ nta_outgoing_t *outgoing_find(nta_agent_t const *sa,
     if (orq->orq_stateless)
       continue;
     /* Accept terminated transactions when looking for original INVITE */
-    
+
     //DCH: this looks like a bug
     //if (orq->orq_terminated && method2 != sip_method_invalid)
-    if (orq->orq_terminated && method2 != sip_method_invite) {
+    if (orq->orq_terminated && method2 != sip_method_invite)
 
       continue;
     if (hash != orq->orq_hash)
