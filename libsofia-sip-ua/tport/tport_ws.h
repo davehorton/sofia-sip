19142a963ed516dd33994d3dfe07bfe60a2b48b0
Dave Horton <daveh@beachdognet.com>
2022-01-06 09:09:05 -0500

    another missed file

--- libsofia-sip-ua/tport/tport_ws.h
+++ libsofia-sip-ua/tport/tport_ws.h
@@ -56,8 +56,10 @@ typedef enum {
 typedef struct tport_ws_s {
   tport_t  wstp_tp[1];
   wsh_t    ws;
-  char    *wstp_buffer;
+  char     wstp_buffer[65536];
+  size_t   wstp_buflen;
   SU_S8_T  ws_initialized;
+  time_t connected;
   unsigned ws_secure:1;
   unsigned:0;
 } tport_ws_t;
