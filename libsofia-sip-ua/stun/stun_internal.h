4ee39cc9759ece7234e625d43ae63853d7132597
khang <khanghugo123@gmail.com>
2022-05-29 18:08:32 -0400

    manually fix everything, bad

--- libsofia-sip-ua/stun/stun_internal.h
+++ libsofia-sip-ua/stun/stun_internal.h
@@ -86,7 +86,13 @@ extern char const STUN_DEBUG[]; /* dummy declaration for Doxygen */
 #define STUN_LIFETIME_CI  5        /**< 5 sec confidence interval */
 #endif
 
+<<<<<<< HEAD
 #define STUN_ERROR(err, what) (SU_DEBUG_5(("%s: %s: %s\n", __func__, #what, su_strerror(err))))
+=======
+#define STUN_ERROR(err, what) \
+  SU_DEBUG_5(("%s: %s: %s\n", __func__, #what, su_strerror(err))), \
+    -1                   \
+>>>>>>> 672dcd72 (manually fix everything, bad)
 
 int stun_is_requested(tag_type_t tag, tag_value_t value, ...);
 
